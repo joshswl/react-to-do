2fc6984645a2aac6fed03821faefc6d7cef7a456
Josh Lee <joshswl@gmail.com>
2018-05-05 15:57:27 -0500

    layout (w/out styles)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteText: '',
+    }
+  }
+  updateNoteText(noteText) {
+    this.setState({ noteText: noteText.target.value })
+  }
   render() {
     return (
       <div className="container">
-         
+        <div className="header">To-Do List</div>
+        <div className="btn">+</div>
+        <input type="text"
+          ref={((input) => {this.textInput = input})}
+          className="textInput"
+          value={this.state.noteText}
+          onChangeText={noteText => this.updateNoteText(noteText)}
+        />
       </div>
     );
   }
