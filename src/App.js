7411f28dfdf279c3970736504a8fcc40dfe509a9
Josh Lee <joshswl@gmail.com>
2018-05-05 17:08:04 -0500

    setup sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Note from './components/Note';
-import './App.css';
+import './styles/App.scss';
 
 class App extends Component {
   constructor(props) {
